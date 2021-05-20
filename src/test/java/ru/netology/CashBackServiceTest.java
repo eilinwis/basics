ddb474eaf66b962d4d0379e673d6c294ac0b500e
Roman Kudryavtsev <TTe4kiH98@gmail.com>
2021-05-20 14:08:05 +0300

    testng checked

---
+++ src/test/java/ru/netology/CashBackServiceTest.java
@@ -0,0 +1,37 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashBackServiceTest {
+    public CashBackService service = new CashBackService();
+
+    @Test
+    public void testRemain() {
+        int amount = 600;
+        service.remain(amount);
+        assertEquals(service.remain(amount), 400);
+    }
+
+    @Test
+    public void testAboveBorderLine() {
+        int amount = 1001;
+        service.remain(amount);
+        assertEquals(service.remain(amount), 999);
+    }
+
+    @Test
+    public void testBelowBorderLine() {
+        int amount = 999;
+        service.remain(amount);
+        assertEquals(service.remain(amount), 1);
+    }
+
+    @Test
+    public void testBorderLine() {
+        int amount = 1000;
+        service.remain(amount);
+        assertEquals(service.remain(amount), 0);
+    }
+}
